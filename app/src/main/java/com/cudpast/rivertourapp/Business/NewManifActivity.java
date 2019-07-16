af0b0d389a6775d545833fb3e2b8f25798159454
Slinkter <luis.j.cueva@gmail.com>
2019-07-16 15:43:36 -0500

    Inicio de RiverTourApp

---
+++ app/src/main/java/com/cudpast/rivertourapp/Business/NewManifActivity.java
@@ -0,0 +1,15 @@
+package com.cudpast.rivertourapp.Business;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cudpast.rivertourapp.R;
+
+public class NewManifActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_manif);
+    }
+}
