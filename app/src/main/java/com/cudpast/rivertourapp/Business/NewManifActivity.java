bcfa86430e4d97945aae542eda17e2609ac0245d
Slinkter <luis.j.cueva@gmail.com>
2019-08-05 17:05:02 -0500

    2019 08 05

--- app/src/main/java/com/cudpast/rivertourapp/Business/NewManifActivity.java
+++ app/src/main/java/com/cudpast/rivertourapp/Business/NewManifActivity.java
@@ -1,11 +1,9 @@
 package com.cudpast.rivertourapp.Business;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -13,23 +11,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cudpast.rivertourapp.Model.Chofer;
+import com.cudpast.rivertourapp.Model.Vehiculo;
 import com.cudpast.rivertourapp.R;
+import com.cudpast.rivertourapp.SQLite.DbHelper;
 
 import java.util.ArrayList;
 
 public class NewManifActivity extends AppCompatActivity {
 
 
-    private TextView guiaVehiculo, guiaN, guiaMatricula, guiaMarca, guiaGuia, guiaFecha, guiaChofer1, guiaChofer2, guiaBrevete1, guiaBrevete2, guiaDestino;
+    private TextView guiaNombreVehiculo, guiaMatricula, guiaMarca, guiaGuia, guiaFecha, guiaChofer1, guiaChofer2, guiaBrevete1, guiaBrevete2, guiaDestino;
     private TextView pasajeroNombre, pasajeroEdad, pasajeroOcupacion, pasajeroNacionalidad, pasajeroN, pasajeroDNI, pasajeroDestino;
     private Button btnGuia;
     private Button btnPasajero;
 
-    private Spinner chofer1, chofer2;
+    private Spinner guiaPlacaVehiculo, chofer1, chofer2;
     ArrayList<String> listaPersonas;
-    ArrayList<Chofer> personasList;
+    ArrayList<Chofer> choferList;
+    ArrayList<Vehiculo> vehiculoList;
 
-
+    DbHelper conn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +38,41 @@ public class NewManifActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_new_manif);
         //
+        conn = new DbHelper(this);
+        //
+        ArrayAdapter<CharSequence> adapter;
+        adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, listaPersonas);
+        guiaPlacaVehiculo.setAdapter(adapter);
+        guiaPlacaVehiculo
+                .setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                        if (position != 0) {
+                            guiaNombreVehiculo.setText(vehiculoList.get(position - 1).getNombrevehiculo());
+                            guiaMatricula.setText(vehiculoList.get(position - 1).getMatriculaVehiculo());
+                            guiaMarca.setText(vehiculoList.get(position - 1).getMarcaVehiculo());
+
+                        } else {
+                            guiaNombreVehiculo.setText(" ");
+                            guiaMatricula.setText(" ");
+                            guiaMarca.setText(" ");
+
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
 
 
         //Bloque 1
-        guiaVehiculo = findViewById(R.id.guiaPlacaVehiculo);
-        guiaN = findViewById(R.id.guiaNombreVehiculo);
+        guiaPlacaVehiculo = findViewById(R.id.guiaPlacaVehiculo);
+        guiaNombreVehiculo = findViewById(R.id.guiaNombreVehiculo);
         guiaMatricula = findViewById(R.id.guiaMatricula);
         guiaMarca = findViewById(R.id.guiaMarca);
         guiaGuia = findViewById(R.id.guiaGuia);
@@ -63,18 +94,18 @@ public class NewManifActivity extends AppCompatActivity {
         guiaDestino = findViewById(R.id.guiaDestino);
 
         btnPasajero = findViewById(R.id.btnPasajero);
-        
+
         btnGuia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(NewManifActivity.this, "Guia button", Toast.LENGTH_SHORT).show();
             }
         });
-        
+
         btnPasajero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(NewManifActivity.this, "Pasajero button", Toast.LENGTH_SHORT).show();
+                Toast.makeText(NewManifActivity.this, "Pasajero button ", Toast.LENGTH_SHORT).show();
             }
         });
 /*
