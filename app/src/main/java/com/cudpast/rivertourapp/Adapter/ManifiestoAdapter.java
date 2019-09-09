fe428d64f6a90edf00a445d14558a18ea6bb2b01
Slinkter <luis.j.cueva@gmail.com>
2019-09-09 10:02:44 -0500

    201909_09 : Lunes , revisión

--- app/src/main/java/com/cudpast/rivertourapp/Adapter/ManifiestoAdapter.java
+++ app/src/main/java/com/cudpast/rivertourapp/Adapter/ManifiestoAdapter.java
@@ -2,11 +2,13 @@ package com.cudpast.rivertourapp.Adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cudpast.rivertourapp.Model.Manifiesto;
 import com.cudpast.rivertourapp.R;
@@ -14,33 +16,64 @@ import com.cudpast.rivertourapp.R;
 import java.util.List;
 
 public class ManifiestoAdapter extends RecyclerView.Adapter<ManifiestoAdapter.customRVManifiesto> {
+    public static final String TAG = ManifiestoAdapter.class.getSimpleName();
     //.Variable
     private List<Manifiesto> mListManifiesto;
+    private OnItemClickListener mItemListener;
 
     //.Constructor
     public ManifiestoAdapter(List<Manifiesto> mListManifiesto) {
         this.mListManifiesto = mListManifiesto;
     }
 
-
     //.Clase RecyclerView
     public class customRVManifiesto extends RecyclerView.ViewHolder {
 
         TextView idGuiaMani, fechaMani, destinoMani, vehiculoMani, choferMani;
-        Button btn_listPasajero;
 
-        public customRVManifiesto(@NonNull View itemView) {
+        ImageView btn_listPasajero, btn_showSyncStatus;
+
+        public customRVManifiesto(@NonNull final View itemView, final OnItemClickListener listener) {
             super(itemView);
             idGuiaMani = itemView.findViewById(R.id.it_idGuia);
             fechaMani = itemView.findViewById(R.id.it_fechaGuia);
             destinoMani = itemView.findViewById(R.id.it_destinoMani);
             vehiculoMani = itemView.findViewById(R.id.it_vehiculoMani);
             choferMani = itemView.findViewById(R.id.it_choferMAni);
-            //      btn_listPasajero = itemView.findViewById(R.id.it_buttonListPasajero);
+            btn_showSyncStatus = itemView.findViewById(R.id.it_btn_showSyncStatus);
+            btn_listPasajero = itemView.findViewById(R.id.it_btn_showListPasajero);
+
+            btn_showSyncStatus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        Log.e(TAG, " " + position);
+                        Toast.makeText(itemView.getContext(), "Pasajero Eliminado " + (position + 1), Toast.LENGTH_SHORT).show();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onShowListManfiesto(position);
+                        }
+                    }
+                }
+            });
+
+            btn_listPasajero.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        Log.e(TAG, " " + position);
+                        Toast.makeText(itemView.getContext(), "Sync " + (position + 1), Toast.LENGTH_SHORT).show();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onShowSync(position);
+                        }
+                    }
+                }
+            });
 
         }
     }
-    //.Clase RecyclerView
+    //.
 
     @NonNull
     @Override
@@ -48,7 +81,7 @@ public class ManifiestoAdapter extends RecyclerView.Adapter<ManifiestoAdapter.cu
         View itemView = LayoutInflater
                 .from(viewGroup.getContext())
                 .inflate(R.layout.item_manifiesto, viewGroup, false);
-        return new customRVManifiesto(itemView);
+        return new customRVManifiesto(itemView, mItemListener);
     }
 
     @Override
@@ -59,7 +92,6 @@ public class ManifiestoAdapter extends RecyclerView.Adapter<ManifiestoAdapter.cu
         holder.destinoMani.setText("Destino : " + manifiesto.getDestinoMani());
         holder.vehiculoMani.setText("Vehiculo : " + manifiesto.getVehiculoMani());
         holder.choferMani.setText("Chofer : " + manifiesto.getChoferMani());
-
     }
 
     @Override
@@ -67,5 +99,15 @@ public class ManifiestoAdapter extends RecyclerView.Adapter<ManifiestoAdapter.cu
         return mListManifiesto.size();
     }
 
+
+    public interface OnItemClickListener {
+        void onShowSync(int position);
+
+        void onShowListManfiesto(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mItemListener = listener;
+    }
 
 }
